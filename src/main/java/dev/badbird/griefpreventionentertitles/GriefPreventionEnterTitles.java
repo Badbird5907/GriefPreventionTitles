879f49eec754b37636ccab8c3c712db8559bbddd
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-06-20 09:45:18 -0400

    finished?

--- src/main/java/dev/badbird/griefpreventionentertitles/GriefPreventionEnterTitles.java
+++ src/main/java/dev/badbird/griefpreventionentertitles/GriefPreventionEnterTitles.java
@@ -2,12 +2,16 @@ package dev.badbird.griefpreventionentertitles;
 
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -21,12 +25,28 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
 
     private static MiniMessage miniMessage;
 
+    private static String enterTitle;
+    private static String enterSubtitle;
+    private static String leaveTitle;
+    private static String leaveSubtitle;
+
     @Override
     public void onEnable() {
         miniMessage = MiniMessage.miniMessage();
         if (!getDataFolder().exists()) getDataFolder().mkdir();
         if (!new File(getDataFolder(), "config.yml").exists()) saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
+
+        String enterBase = "titles.enter";
+        if (getConfig().getBoolean(enterBase + ".enabled")) {
+            enterTitle = getConfig().getString(enterBase + ".title");
+            enterSubtitle = getConfig().getString(enterBase + ".subtitle");
+        }
+        String leaveBase = "titles.leave";
+        if (getConfig().getBoolean(leaveBase + ".enabled")) {
+            leaveTitle = getConfig().getString(leaveBase + ".title");
+            leaveSubtitle = getConfig().getString(leaveBase + ".subtitle");
+        }
     }
 
     @EventHandler
@@ -38,6 +58,7 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
     public void onJoin(PlayerJoinEvent e) {
         claimMap.put(e.getPlayer().getUniqueId(), GriefPrevention.instance.dataStore.getClaimAt(e.getPlayer().getLocation(), true, null));
     }
+
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
         claimMap.remove(event.getPlayer().getUniqueId());
@@ -47,7 +68,25 @@ public final class GriefPreventionEnterTitles extends JavaPlugin implements List
         Claim cachedClaim = claimMap.get(player.getUniqueId());
         Claim movingTo = GriefPrevention.instance.dataStore.getClaimAt(from, true, cachedClaim);
         if (cachedClaim == null && movingTo != null) { //Entering a claim
-
+            Component enter = null, sub = null;
+            if (enterTitle != null && !enterTitle.isEmpty()) {
+                enter = miniMessage.deserialize(enterTitle);
+            } else enter = Component.empty();
+            if (enterSubtitle != null && !enterSubtitle.isEmpty()) {
+                sub = miniMessage.deserialize(enterSubtitle);
+            } else sub = Component.empty();
+            Title title = Title.title(enter, sub);
+            player.showTitle(title);
+        } else if (cachedClaim != null && movingTo == null) { //Leaving a claim
+            Component leave = null, sub = null;
+            if (leaveTitle != null && !leaveTitle.isEmpty()) {
+                leave = miniMessage.deserialize(leaveTitle);
+            } else leave = Component.empty();
+            if (leaveSubtitle != null && !leaveSubtitle.isEmpty()) {
+                sub = miniMessage.deserialize(leaveSubtitle);
+            } else sub = Component.empty();
+            Title title = Title.title(leave, sub);
+            player.showTitle(title);
         }
     }
 
